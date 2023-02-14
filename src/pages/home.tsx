c6bfd59fc9ca0ce7ca182b0b7493b563b3a86f4c
Alex Nascimento <alexdavi150@gmail.com>
2023-02-13 21:47:26 -0300

    Prepare the file structure

---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import '../styles/global.css'
+import '../styles/home.css'
+
+export default function Home() {
+  return (
+    <div className='container'>Home</div>
+  )
+}
